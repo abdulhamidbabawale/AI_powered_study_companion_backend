343ad401857fb32722c9d9dbae90d936e377e280
abdulhamidbabawale <hamidbabawale@gmail.com>
2026-05-01 17:04:13 +0100

    refractor(chat): removed unused fields and schemas

--- app/schemas/chat_schemas.py
+++ app/schemas/chat_schemas.py
@@ -2,20 +2,10 @@ from pydantic import BaseModel,Field
 from datetime import datetime
 from typing import Literal, Optional, List
 
-# class CreateChatSchema(BaseModel):
-#     user_id: str
-#     title: Optional[str] = None
-
-# class AddMessageSchema(BaseModel):
-#     role:Literal["user", "assistant", "system"]
-#     content: str
-
 class CreateChatSchema(BaseModel):
-    user_id: str
     message: str
 
 class AddMessageSchema(BaseModel):
-    user_id: str
     message: str
     chat_id: str 
 
@@ -28,23 +18,5 @@ class SummaryRequest(BaseModel):
     content: str
 
 class QuestionRequest(BaseModel):
-    user_id: str
-    content: str
     question: str
-    chat_id: Optional[str] = None
 
-# class Message(BaseModel):
-#     role: Literal["user", "assistant", "system"]
-#     content: str
-#     timestamp: datetime = datetime.utcnow()
-
-# class Chat(BaseModel):
-#     id: Optional[str] = Field(alias="_id")
-#     user_id: str
-#     title: Optional[str] = None
-#     messages: List[Message] = []
-#     created_at: datetime = datetime.utcnow()
-#     updated_at: datetime = datetime.utcnow()
-
-#     class Config:
-#         populate_by_name = True
