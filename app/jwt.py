92c6b0f8a5ff1d7a9bdc6b60a95b154fb47efdc5
abdulhamidbabawale <hamidbabawale@gmail.com>
2026-05-01 16:50:08 +0100

    feat: made middleware to get current user

--- app/jwt.py
+++ app/jwt.py
@@ -1,13 +1,16 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
-from fastapi import HTTPException
-import secrets
+from fastapi import HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import os
+from dotenv import load_dotenv
+load_dotenv()  
 
-# secrets.token_hex()
-
-SECRET_KEY = "f9bf78b9a18ce6d46a0cd2b0b86df9da"  # move to .env later
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+security=HTTPBearer()
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
@@ -19,5 +22,16 @@ def decode_access_token(token: str) -> dict:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials  
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if not user_id:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return user_id
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid or expired token")
